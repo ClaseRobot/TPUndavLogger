03d36007d17e89a89b38641d4800cbdd8969b464
santiago alessio <santydzs2@gmail.com>
2019-04-28 15:00:27 -0300

    subida de mail

--- src/TPLoggerUndav.cpp
+++ src/TPLoggerUndav.cpp
@@ -7,11 +7,24 @@
 //============================================================================
 
 #include <iostream>
-using namespace std;
+#include "Email.h"
+
+using std::cout;
+using std::endl;
+using namespace EmailUndav;
 
 int main() {
 	cout << "das11das" << endl; // prints
 	cout << "nueva linea de test"<<endl;
-	cout << "esto vino del puysh"<<endl;
+	cout << "pepe"<<endl;
 	return 0;
+}
+
+void PruebasUnitariasEmail(){
+	Email* correo = CrearMail("Parcial", "El Sábado 4 de Mayo es el parcial. Estudiar todos los temas vistos hasta el 25 de Abril.");
+	CambiarAsunto(correo, "Evaluación de Avance");
+	AgregarDestinatario(correo, "gabytubio@gmail.com");
+	AgregarDestinatario(correo, "jjlopez@gmail.com");
+	MostrarEmail(correo);
+	DestruirEmail(correo);
 }
