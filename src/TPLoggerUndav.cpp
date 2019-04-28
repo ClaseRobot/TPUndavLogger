3fafc377bc09b68d36fe1a40a7de9401631cdad1
santiago alessio <santydzs2@gmail.com>
2019-04-28 03:03:22 -0300

    initial commit

---
+++ src/TPLoggerUndav.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : TPLoggerUndav.cpp
+// Author      : 
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "das11das" << endl; // prints
+	return 0;
+}
